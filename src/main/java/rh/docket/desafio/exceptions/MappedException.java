a0c686f3acea73e7a16db55d97290f980b0e6ebd
pradojonas <pradojonas64@gmail.com>
2022-02-21 09:32:34 -0300

    Create MappedException.java

---
+++ src/main/java/rh/docket/desafio/exceptions/MappedException.java
@@ -0,0 +1,19 @@
+package rh.docket.desafio.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class MappedException extends Exception {
+    private static final long serialVersionUID = 1L;
+    
+    private HttpStatus statusCode;
+
+    public MappedException(String string, HttpStatus statusCode) {
+        super(string);
+        this.statusCode = statusCode;
+    }
+
+    public HttpStatus getStatusCode() {
+        return this.statusCode;
+    }
+
+}
